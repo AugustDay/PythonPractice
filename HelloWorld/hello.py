aca6e7132748e8ffa31ab1b8c89f7577526ad819
AugustDay <augustday.adi@gmail.com>
2016-05-04 23:24:21 -0700

    added some things

--- HelloWorld/hello.py
+++ HelloWorld/hello.py
@@ -1,1 +1,13 @@
-print "hello world!"
+#Austin Ingraham
+#simple Python syntax practice
+
+def main():
+	print("Hello world!")
+	helloTest()
+	print("Printing on same line works...", end=" ")
+	print("Like this!")
+
+def helloTest(): 
+	print("Hello Function.\nThis is a test.")	
+
+main()
