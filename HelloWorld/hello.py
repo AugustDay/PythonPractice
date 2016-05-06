6415646de0293d1cc80c271039e4d237326b21b4
AugustDay <augustday.adi@gmail.com>
2016-05-05 21:40:53 -0700

    more print practice + simple command line args

--- HelloWorld/hello.py
+++ HelloWorld/hello.py
@@ -1,13 +1,21 @@
 #Austin Ingraham
 #simple Python syntax practice
 
+import sys
+
 def main():
 	print("Hello world!")
 	helloTest()
 	print("Printing on same line works...", end=" ")
 	print("Like this!")
+	if len(sys.argv) > 1:
+		print("Number of arguments:", len(sys.argv))
+		print("Hello, {}!".format(sys.argv[1]))
+	
 
 def helloTest(): 
 	print("Hello Function.\nThis is a test.")	
+
+	
 
 main()
