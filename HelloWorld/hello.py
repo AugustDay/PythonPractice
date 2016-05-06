88daa375333458b69ea5f6873924dec9d1879273
AugustDay <augustday.adi@gmail.com>
2016-05-06 09:08:49 -0700

    testing repr() function

--- HelloWorld/hello.py
+++ HelloWorld/hello.py
@@ -7,15 +7,19 @@ def main():
 	print("Hello world!")
 	helloTest()
 	print("Printing on same line works...", end=" ")
-	print("Like this!")
+	print("like this!")
 	if len(sys.argv) > 1:
 		print("Number of arguments:", len(sys.argv))
 		print("Hello, {}!".format(sys.argv[1]))
-	
+		
+	alternateStringFormatting()
 
 def helloTest(): 
 	print("Hello Function.\nThis is a test.")	
 
-	
+def alternateStringFormatting():
+	s = "repr\tfunction"
+	print(s)
+	print(repr(s))
 
 main()
